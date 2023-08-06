e488273804b5c26a58727711bddc25e6cce14cf6
Rob Bock <rbock9@gmail.com>
2023-08-06 13:54:16 -0400

    classic fizzbuzz warmup exercise

---
+++ js_exercises/fizzbuzz.js
@@ -0,0 +1,36 @@
+// Classic Fizzbuzz
+
+// For Loop in a function
+// function fizzbuzz() {
+//     for (let i = 1; i < 100; i++) {
+//         if (i % 3 == 0) {
+//             console.log("fizz")
+//         }
+//         else if (i % 5 == 0) {
+//             console.log("buzz")
+//         }
+//         else if (i % 3 == 0 && i % 5 == 0) {
+//             console.log("fizzbuzz")
+//         }
+//         else (console.log(i))
+//     }
+// }
+
+// console.log(fizzbuzz())
+
+
+// While Loop
+// let result = 0;
+// while  (result < 100) {
+//     result ++;
+//     if (result % 15 == 0) {
+//         console.log("fizzbuzz")
+//     }
+//     else if (result % 5 == 0) {
+//         console.log("buzz")
+//     }
+//     else if (result % 3 == 0) {
+//         console.log("fizz")
+//     }
+//     else (console.log(result))
+// }
